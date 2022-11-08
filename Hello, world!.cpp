c79c0e34b57329c26c257ec8743631543a7edcfd
shakilahamedriaz <shakilahamedriaz@gmail.com>
2022-11-08 18:56:20 +0600

    done

---
+++ Hello, world!.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int min()
+{
+
+
+    cout<<"Hello, World!"<<endl;
+
+    return 0;
+
+}
---
+++ loop.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+
+    int 1=one,2=two,3=three,4=four,5=five,6=six,7=seven,9=nine;
+
+    for(int i=a; i<b; i++){
+
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
+
