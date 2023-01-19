88a4bac10df2eba504dcdd8073511a556597912c
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-01-19 21:16:25 +0600

    done

--- Min Max sum.cpp
+++ Min Max sum.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+
+        cin>>arr[i];
+    }
+
+    long minsum=0,maxsum=0;
+
+    for(int i=0; i<4; i++){
+
+        minsum+=arr[i];
+        maxsum+=arr[n-1-i];
+    }
+
+
+    cout<<minsum<< " " <<maxsum<<endl;
+}
+
 
